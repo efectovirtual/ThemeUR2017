ac8b154355128daf5647a1cb5acd4793704398d4
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-08 01:02:55 -0500

    StyleResponsive InternalPage

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -8,24 +8,23 @@
       'post_type' => 'slider',
       'orderby' => 'menu_order',
       'order' => 'ASC',
-      'posts_per_page' => -1); 
+      'posts_per_page' => -1);
     ?>
     <?php $slider = new WP_Query($args);?>
-    
 
-  
-    <?php while($slider->have_posts()): 
-          $slider->the_post(); 
-          $image = get_field('imagen'); 
+
+
+    <?php while($slider->have_posts()):
+          $slider->the_post();
+          $image = get_field('imagen');
     ?>
 
       <div class="owl-slide" style="background: url(<?php echo $image['url']; ?>) center top no-repeat;">
           <img class="item" src="<?php echo get_template_directory_uri(); ?>/img/bg.png" />
-      </div>  
+      </div>
 
   <?php endwhile; wp_reset_postdata(); ?>
 
 
   </div>
 </div>
-
